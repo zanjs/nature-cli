46ebcc3a9c20ffe2792e77c55f1a9b9ec8683f9e
ZanJS <suihuasheng@gmail.com>
2019-11-14 11:50:28 +0800

    :cat:

---
+++ index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const {plugins, external, globals} = require('./rollup.config')
+const rollup = require('rollup')
+const watch = require('watch')
+const notice = require('notice-core')
+const path = require('path')
+const pkg = require(path.resolve('package.json'))
+
+let caches = {}
+
+const build = () => {
+  // 按 umd、cjs 包格式构建
+  // umd 需要把依赖包打进目标文件
+  // cjs 需要将依赖包申明为外部依赖
+  ['umd', 'cjs'].forEach(async format => {
+    const isUMD = format === 'umd'
+    const inputOptions = {
+      input: 'src/index.js',
+      external: isUMD ? [] : external,
+      cache: caches[format],
+      plugins
+    }
+    const outputOptions = {
+      format,
+      globals: isUMD ? [] : globals,
+      sourcemap: true,
+      name: pkg.moduleName || pkg.name,
+      file: `dist/index${isUMD ? '' : '.cjs'}.js`
+    }
+
+    const bundle = await rollup.rollup(inputOptions)
+
+    caches[format] = bundle
+
+    await bundle.write(outputOptions)
+
+    notice.success(`The ${format} format package built successfully!`)
+  })
+}
+
+if (process.env.NODE_ENV === 'production') {
+  build()
+} else {
+  // 监听 src 文件夹下的文件变更
+  watch.watchTree('src', (f, curr, prev) => {
+    if (typeof f === 'object' && prev === null && curr === null) {
+      notice.info('Start watching file changes, you can press "ctrl+c" to cancel.')
+      build()
+    } else if (prev === null) {
+    } else if (curr.nlink === 0) {
+    } else {
+      notice.info('File changes, start building.')
+      build()
+    }
+  })
+}
---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const uglify = require("rollup-plugin-uglify").uglify;
+const commonjs = require('rollup-plugin-commonjs')
+const babel = require('rollup-plugin-babel');
+const resolve = require('rollup-plugin-node-resolve');
+const postcss = require('rollup-plugin-postcss')
+const json = require('@rollup/plugin-json')
+const postcssNesting = require('postcss-nesting');
+const simpleVars = require('postcss-simple-vars');
+const postcssPresetEnv = require('postcss-preset-env');
+const url = require("postcss-url")
+const pkg = require(path.resolve('package.json'))
+const external = Object.keys(pkg.dependencies)
+
+const plugins = [,
+  json({
+    include: 'src/**',
+    exclude: ['node_modules']
+  }),
+  postcss({
+    plugins: [
+      simpleVars(),
+      postcssNesting(),
+      postcssPresetEnv(),
+      require('cssnano')(),
+      url({
+        url: 'inline'
+      }),
+      // require('postcss-modules')({
+      //   generateScopedName: '[local]__[hash:base64:5]'
+      // })
+    ],
+    extensions: ['.scss']
+  }),
+  resolve({
+    jsnext: true,
+    main: true,
+    browser: true
+  }),
+  commonjs(),
+  babel({
+    exclude: 'node_modules/**',
+    runtimeHelpers: true
+  }),
+  uglify()
+]
+
+module.exports = {
+  plugins,
+  external,
+  globals: Object.keys(external).map(value => ({[value]: value}))
+}
